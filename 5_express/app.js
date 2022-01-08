83b86edbdb954ca384c12a9bf5d1886e5afae52d
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-01-08 17:13:05 +0530

    third party package installed

--- 5_express/app.js
+++ 5_express/app.js
@@ -4,19 +4,37 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next();    // Allows request to continue to next middleware in line
-});
+// app.use((req, res, next) => {
+//   console.log('In the middleware!');
+//   next();    // Allows request to continue to next middleware in line
+// });
 //use() allows us to add new middleware function
-// it takes the list of argumrnts
+// it takes the list of argumrnts 
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express</h1>')
-  //....
+app.use('/', (req, res, next) => {
+  // console.log('This always runs!');
+  next();
 });
 
-const server = http.createServer(app);
+app.use('/add-product', (req, res, next) => {
+  // console.log('This runs!');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+});
 
-server.listen(300);
+
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  // console.log('In another middleware!');
+  res.send('<h1>Hello from Express</h1>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(300);
+
+app.listen(300);
