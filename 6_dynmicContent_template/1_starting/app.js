53e1e5c672f1a600e6be9ef02671ab25f9f5ca9c
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-01-29 05:17:51 +0530

    starting 6th module

---
+++ 6_dynmicContent_template/1_starting/app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+ // use() can use any http method 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+//as we used post method in shop.js, order doesn't matter
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(300) ;
