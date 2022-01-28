a070b8683f9543bd5881424c2fce247ebb577435
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-01-28 12:21:56 +0530

    some router updates

--- 5_express/2_express_router/app.js
+++ 5_express/2_express_router/app.js
@@ -6,10 +6,15 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+ // use() can use any http method 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 //as we used post method in shop.js, order doesn't matter
 app.use(shopRoutes);
 
-app.listen(300);
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
+
+app.listen(300) ;
