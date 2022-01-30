2dc1cfff0049506465a047e4a6304241b95f1b29
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-01-30 08:55:30 +0530

    adding main-layout

---
+++ 6_dynmicContent_template/3_handlebars/2_adding_main-layout/app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expHbs = require('express-handlebars')
+
+const app = express();
+
+app.engine('hbs', expHbs.engine({
+  layoutsDir: 'views/layouts/',
+  defaultLayout: 'main-layout',
+  extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+ 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+ // use() can use any http method 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+//as we used post method in shop.js, order doesn't matter
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
+})
+
+app.listen(300);
