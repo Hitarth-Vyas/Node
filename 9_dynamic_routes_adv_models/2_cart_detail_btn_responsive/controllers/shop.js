a8091e6479e78a9698ac11e17bbae0b1c700f83f
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-02-03 14:05:39 +0530

    making cart and details button responsive

---
+++ 9_dynamic_routes_adv_models/2_cart_detail_btn_responsive/controllers/shop.js
@@ -0,0 +1,64 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products, 
+      pageTitle: 'Shop', 
+      path: '/products',
+    });
+  });
+}
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  // console.log(prodId);
+  Product.findById(prodId, product => {
+    // console.log(product);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
+  })
+  // res.redirect('/');
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products, 
+      pageTitle: 'Shop', 
+      path: '/',
+    });
+  });
+}
+
+exports.getOrders = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/orders', {
+      pageTitle: 'Your Orders', 
+      path: '/orders',
+    });
+  });
+}
+
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+      pageTitle: 'Your Cart', 
+      path: '/cart',
+  });
+}
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+}
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout', 
+    path: '/checkout',
+});
+}
