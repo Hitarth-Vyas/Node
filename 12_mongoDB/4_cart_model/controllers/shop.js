d8bea73e0917bb87b677f38abfe6b7aeea3aea04
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-03-27 23:17:40 +0530

    Displaying cart items

--- 12_mongoDB/4_cart_model/controllers/shop.js
+++ 12_mongoDB/4_cart_model/controllers/shop.js
@@ -44,17 +44,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
+    .then(products => {
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
             products: products
-          });
         })
-        .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 };
@@ -63,10 +58,11 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-    return req.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then(result => {
       console.log(result);
+      res.redirect('/cart');
     });
 };
 
