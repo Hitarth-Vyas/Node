1d08b2a26684f04a5db74e50a13b4750987efcb4
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-03-29 18:42:21 +0530

    updating products

--- 13_mongoose/1_connect_db/controllers/admin.js
+++ 13_mongoose/1_connect_db/controllers/admin.js
@@ -61,15 +61,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(
-    updatedTitle, 
-    updatedPrice, 
-    updatedDesc, 
-    updatedImageUrl, 
-    new ObjectId(prodId)
-  )
-  product
-    .save()
+  
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save()
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -78,7 +78,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
