8eb76faa422bd7c99c04205d14d55f34d0d90114
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-05-25 12:13:27 +0530

    users intro

--- 13_mongoose/3_user_model/controllers/admin.js
+++ 13_mongoose/3_user_model/controllers/admin.js
@@ -11,17 +11,19 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = async(req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  
+  const user = await  User.findById('62453462bea3af822d641f86')
   const product = new Product({
     title: title,
     price: price,
     description: description,
     imageUrl: imageUrl,
-    userId: req.user
+    userId: user
   });
   product
     .save()
