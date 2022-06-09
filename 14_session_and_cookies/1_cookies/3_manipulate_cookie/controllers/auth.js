e1a122b8265097d62d2259d3d256d225821c7834
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2022-06-09 14:33:18 +0530

    manipulate cookies

---
+++ 14_session_and_cookies/1_cookies/3_manipulate_cookie/controllers/auth.js
@@ -0,0 +1,18 @@
+exports.getLogin = (req, res, next) => {
+  // console.log(req.get('Cookie').split(';')[1].trim().split('=')[1]);
+  // console.log(req.get('Cookie').trim().split('=')[1]);
+  const isLoggedIn = req
+    .get('Cookie')
+    .trim()
+    .split('=')[1] === 'true';
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: isLoggedIn
+  })
+}
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true')
+  res.redirect('/');
+}
